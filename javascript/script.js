bfd248946f8513e11c722c241384ddd91de446ee
Matheus Jorge <matheusjorgealves09@gmail.com>
2026-04-17 13:34:13 -0300

    adicionando interatividade ao site

---
+++ javascript/script.js
@@ -0,0 +1,64 @@
+function clickMenu() { /* função para alterar o menu  */
+    if (menu.style.display == 'block') {
+        menu.style.display = 'none'
+    } else {
+        menu.style.display = 'block'
+    }
+}
+
+
+function mudouTamanho () { /* função para esconder ou mostrar o menu */
+    if (window.innerWidth >= 768) {
+        menu.style.display = 'block'
+    } else {
+        menu.style.display = 'none'
+    }
+}
+
+
+/* CONFIGURAÇÕES PARA O CARRINHO DE COMPRAS */
+let carrinho = [];
+function adicionarItem(nome, preco) { /* função para adicionar item */
+    carrinho.push({
+        nome: nome,
+        preco: preco
+    });
+
+    mostrarCarrinho();
+}
+
+
+function mostrarCarrinho() { /* Faz o carrinho aparecer */
+    let div = document.getElementById("carrinho-itens");
+
+    if (!div) return;
+
+    div.innerHTML = "";
+
+    let total = 0;
+
+    carrinho.forEach((item, index) => {
+        div.innerHTML += 
+            `<p>
+                ${item.nome} - R$ ${item.preco}
+                <button onclick="removerItem(${index})">X</button>
+            </p>`
+        ;
+        total += item.preco;
+    });
+
+    div.innerHTML += `<strong>Total: R$ ${total}</strong>`;
+}
+
+
+function removerItem(index) { /* remove item do carrinho */
+    carrinho.splice(index, 1);
+    mostrarCarrinho();
+}
+
+
+function limparCarrinho() { /* exclui todos os itens do carrinho */
+    carrinho = [];
+    mostrarCarrinho();
+}
+/* FIM DE CONFIGURAÇÕES PARA CARRINHO DE COMPRAS */
