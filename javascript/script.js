cddafafd40c284d8da96471eb83a8cd776b1988b
Matheus Jorge <matheusjorgealves09@gmail.com>
2026-04-17 15:00:30 -0300

    eu fiz diversas correções para conseguir alinhar o menu do site

--- javascript/script.js
+++ javascript/script.js
@@ -1,15 +1,15 @@
 function clickMenu() { /* função para alterar o menu  */
-    if (menu.style.display == 'block') {
+    if (menu.style.display == 'flex') {
         menu.style.display = 'none'
     } else {
-        menu.style.display = 'block'
+        menu.style.display = 'flex'
     }
 }
 
 
 function mudouTamanho () { /* função para esconder ou mostrar o menu */
     if (window.innerWidth >= 768) {
-        menu.style.display = 'block'
+        menu.style.display = 'flex'
     } else {
         menu.style.display = 'none'
     }
